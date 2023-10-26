03a3ed33ac117ee3ca8efd314da768c4e3e2f02a
ByCooper <kup.official@yahoo.com>
2023-10-26 21:35:27 +0300

    Telegram_bot(скелет)

---
+++ src/test/java/ByCooper/tele/ru/telegram_bot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ByCooper.tele.ru.telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
